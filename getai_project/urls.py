6d05faa2166b63b79a774fcd44b3a15e27d17d15
ravalika78 <ravalikavallem4@gmail.com>
2025-07-28 23:40:29 +0530

    Add getai_project/urls.py 1753726229312

--- getai_project/urls.py
+++ getai_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("jobs.urls")),
+    path("", include("jobs.api_urls")),
 ]
 
 if settings.DEBUG:
