9ae11335e61d9032d86fee87b1442de1d83abaf6
ravalika78 <ravalikavallem4@gmail.com>
2025-07-28 23:40:19 +0530

    Add jobs/models/__init__.py 1753726219165

---
+++ jobs/models/__init__.py
@@ -0,0 +1,15 @@
+from .education import Education
+from .experience import Experience
+from .register import Register
+from .resume import Resume
+from .skills import Skills
+from .userdetails import UserDetails
+
+__all__ = [
+    'Education',
+    'Experience', 
+    'Register',
+    'Resume',
+    'Skills',
+    'UserDetails'
+]
